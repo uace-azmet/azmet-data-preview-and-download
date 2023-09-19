7407878d359dd3b282a93d9d4611de7eb56c16a6
Jeremy Weiss <jlweiss@email.arizona.edu>
2023-09-18 17:08:56 -0700

    <style> specific to this app into CSS file

--- app/R/fxnTableCaption.R
+++ app/R/fxnTableCaption.R
@@ -5,6 +5,9 @@
 
 
 fxnTableCaption <- function(timeStep) {
+  todayDate <- gsub(" 0", " ", format(lubridate::today(), "%B %d, %Y"))
+  todayYear <- lubridate::year(lubridate::today())
+  
   webpageAZMet <- a(
     "AZMet website", 
     href="https://staging.azmet.arizona.edu/", 
@@ -31,37 +34,27 @@ fxnTableCaption <- function(timeStep) {
     target="_blank"
   )
  
-  # Hourly
   if (timeStep == "Hourly") {
     apiURL <- a(
       "api.azmet.arizona.edu", 
-      href="https://api.azmet.arizona.edu/v1/observations/hourly", 
+      href="https://api.azmet.arizona.edu/v1/observations/hourly", # Hourly data
       target="_blank"
     )
-    
-    tableCaption <- htmltools::HTML(
-      "<p style='color: #343a40; font-weight: plain; margin-top: 0;'>", 
-      paste0(timeStep, " ", "AZMet data are from", " ", apiURL, ".", " ", "Table values from recent dates may be based on provisional data", ".", " ", "More information about", " ", webpageDataVariables, ",", " ", webpageNetworkMap, ",", " ", "and", " ", webpageStationMetadata, " ", "is available on the", " ", webpageAZMet, ".", " ", "Users of AZMet data and data applications assume all risks of its use", "."),
+  } else {
+      apiURL <- a(
+        "api.azmet.arizona.edu", 
+        href="https://api.azmet.arizona.edu/v1/observations/daily", # Daily data
+        target="_blank"
+      )
+    }
+  
+  tableCaption <- htmltools::HTML(
+    paste0(timeStep, " ", "AZMet data are from", " ", apiURL, ".", " ", "Table values from recent dates may be based on provisional data", ".", " ", "More information about", " ", webpageDataVariables, ",", " ", webpageNetworkMap, ",", " ", "and", " ", webpageStationMetadata, " ", "is available on the", " ", webpageAZMet, ".", " ", "Users of AZMet data and data applications assume all risks of its use", "."),
+    "<br><br>",
+    paste0("To cite the above AZMet data, please use: 'Arizona Meteorological Network (", todayYear, ") Arizona Meteorological Network (AZMet) Data. https:://azmet.arizona.edu. Accessed", " ", todayDate, "'"),
     "<br><br>",
     paste0("For information on how this webpage is put together, please visit the", " ", webpageCode, " ", "for this tool.")
     )
-  }
-  
-  # Daily
-  if (timeStep == "Daily") {
-    apiURL <- a(
-      "api.azmet.arizona.edu", 
-      href="https://api.azmet.arizona.edu/v1/observations/daily", 
-      target="_blank"
-    )
-    
-    tableCaption <- htmltools::HTML(
-      "<p style='color: #343a40; font-weight: plain; margin-top: 0;'>", 
-      paste0(timeStep, " ", "AZMet data are from", " ", apiURL, ".", " ", "Table values from recent dates may be based on provisional data", ".", " ", "More information about", " ", webpageDataVariables, ",", " ", webpageNetworkMap, ",", " ", "and", " ", webpageStationMetadata, " ", "is available on the", " ", webpageAZMet, ".", " ", "Users of AZMet data and data applications assume all risks of its use", "."),
-      "<br><br>",
-      paste0("For information on how this webpage is put together, please visit the", " ", webpageCode, " ", "for this tool.")
-    )
-  }
   
   return(tableCaption)
 }
